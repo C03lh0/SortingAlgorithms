bb9586daae5dfcfb7c0c83e4a58d85a45af2ea47
Matheus Coelho Muniz dos Santos <macomusa14@gmail.com>
2022-06-01 12:12:30 -0300

    Add file MergeSortMain

---
+++ src/MergeSortMain.java
@@ -0,0 +1,21 @@
+import MergeSort.MergeSort;
+
+public class MergeSortMain {
+
+    public static void run() {
+        
+        int arrayInteger[] = {23, 17, 8, 15, 9, 12, 19, 7};
+
+        System.out.print("Vetor Inicial   ");
+        for(int i = 0; i < arrayInteger.length; i++){
+            System.out.print(arrayInteger[i]+" ");
+        }
+
+        MergeSort.mergeSort(arrayInteger, 0, arrayInteger.length-1);
+
+        System.out.print("\nVetor Final   ");
+        for(int i = 0; i < arrayInteger.length; i++){
+            System.out.print(arrayInteger[i]+" ");
+        }
+    }
+}
